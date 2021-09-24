5e9439af44962e419ca7bf5ccf6c09a724d408a9
Anouar <anouar.hilali1@gmail.com>
2021-09-24 16:55:42 +0100

    Fixed login method

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
+import { HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +11,9 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   isLoginMode = true;
+  isLoading = true;
 
-  constructor() {}
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
@@ -18,7 +22,28 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
+    const username = form.value.username;
+    const password = form.value.password;
+
+    const body = new HttpParams()
+      .set('username', username)
+      .set('password', password)
+      .set('grant_type', 'password');
+
+    if (this.isLoginMode) {
+      this.auth.login(body.toString()).subscribe(
+        (data) => {
+          window.sessionStorage.setItem('token', JSON.stringify(data));
+          console.log(window.sessionStorage.getItem('token'));
+          this.router.navigate(['drugs']);
+        },
+        (error) => {
+          alert(error.error.error_description);
+        }
+      );
+    }
     console.log(form);
     form.reset();
   }
+
 }
