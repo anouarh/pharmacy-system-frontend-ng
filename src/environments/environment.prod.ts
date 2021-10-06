729e83d4cff11a650a74c1b298c15d7540bafa35
Anouar <anouar.hilali1@gmail.com>
2021-10-06 20:32:56 +0100

    Edits for heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://blooming-ravine-65676.herokuapp.com/',
+  authUrl: 'https://blooming-ravine-65676.herokuapp.com/oauth/token',
 };
