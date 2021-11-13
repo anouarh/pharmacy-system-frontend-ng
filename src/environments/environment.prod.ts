7b70426d0e93773b9bbb6de48d297ee3abac5665
Anouar <anouar.hilali1@gmail.com>
2021-11-13 20:24:14 +0100

    Changed servers url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,8 @@ export const APP_CONFIG = {
 };
 export const environment = {
   production: true,
-  api: 'https://blooming-ravine-65676.herokuapp.com/',
-  authUrl: 'https://blooming-ravine-65676.herokuapp.com/oauth/token',
+  //api: 'https://blooming-ravine-65676.herokuapp.com/',
+  //authUrl: 'https://blooming-ravine-65676.herokuapp.com/oauth/token',
+  api: 'http://localhost:8080/',
+  authUrl: 'http://localhost:8080/oauth/token',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,12 @@
 export const APP_CONFIG = {
   production: false,
-  environment: 'LOCAL'
+  environment: 'LOCAL',
 };
 
 export const environment = {
   production: false,
-  api: "https://blooming-ravine-65676.herokuapp.com/",
-  authUrl: "https://blooming-ravine-65676.herokuapp.com/oauth/token",
+  //api: "https://blooming-ravine-65676.herokuapp.com/",
+  //authUrl: "https://blooming-ravine-65676.herokuapp.com/oauth/token",
+  api: 'http://localhost:8080/',
+  authUrl: 'http://localhost:8080/oauth/token',
 };
